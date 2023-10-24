47eadeab5346656758e2beec5d406ab18f0ad20d
Subhan <subhanihsan3@gmail.com>
2023-10-24 12:29:12 +0500

    TC-06 Contact Us Form

---
+++ page-objects/contactus.js
@@ -0,0 +1,49 @@
+import { faker } from "@faker-js/faker";
+import { expect } from "playwright/test";
+
+const name = faker.internet.userName();
+const email = faker.internet.email();
+const subject = faker.internet.displayName();
+const message = faker.internet.password({ length: 20, prefix: "Hello" });
+
+export class Contactus {
+    constructor(page) {
+        this.page = page;
+        this.contactusButton = page.locator('[href="/contact_us"]');
+        this.getinTouchText = page.getByText("Get In Touch");
+        this.nameInputField = page.locator('[data-qa="name"]');
+        this.emailInputField = page.locator('[data-qa="email"]');
+        this.subjectInputField = page.locator('[data-qa="subject"]');
+        this.messageInputField = page.locator('[data-qa="message"]');
+        this.chooseFileButton = page.locator('[name="upload_file"]');
+        this.submitButton = page.locator('[data-qa="submit-button"]');
+        this.successMessageText = page
+            .locator("#contact-page")
+            .getByText(
+                "Success! Your details have been submitted successfully."
+            );
+    }
+
+    contactusFormFill = async () => {
+        await this.contactusButton.waitFor();
+        await this.contactusButton.click();
+        await this.page.waitForURL("/contact_us");
+        await expect(this.getinTouchText).toHaveText("Get In Touch");
+        await this.nameInputField.fill(name);
+        await this.emailInputField.fill(email);
+        await this.subjectInputField.fill(subject);
+        await this.messageInputField.fill(message);
+        await this.chooseFileButton.setInputFiles(
+            "D:/ecommerceDev/upload/image.png"
+        );
+        // handling alert box
+        this.page.on("dialog", (dialog) => {
+            console.warn(dialog.message());
+            dialog.accept();
+        });
+        await this.submitButton.click();
+        await expect(this.successMessageText).toHaveText(
+            "Success! Your details have been submitted successfully."
+        );
+    };
+}
--- page-objects/pageManager.js
+++ page-objects/pageManager.js
@@ -1,6 +1,7 @@
 import { BasePage } from "./basePage";
 import { RegisterUser } from "./registerUser";
 import { Login } from "./login";
+import { Contactus } from "./contactus";
 
 export class PageManager {
     constructor(page) {
@@ -8,6 +9,7 @@ export class PageManager {
         this.basePage = new BasePage(this.page);
         this.registerUser = new RegisterUser(this.page);
         this.userLogin = new Login(this.page);
+        this.contactus = new Contactus(this.page);
     }
 
     navigateToHomePage = async () => {
@@ -20,5 +22,9 @@ export class PageManager {
 
     login = async () => {
         return this.userLogin;
+    };
+
+    contactUS = async () => {
+        return this.contactus;
     };
 }
